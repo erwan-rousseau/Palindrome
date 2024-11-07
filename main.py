12ec905ad782f134f8c65b8829adcb20e8d49a7c
erwan-rousseau <erwan.rousseau@edu.esiee.fr>
2024-11-07 10:01:32 +0000

    Exercice Palindrom Erwan Rousseau G9

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+"""
+"Code permettant de vérifier si mot est un palindrome
+"""
 #### Fonction secondaire
 
 
-def ispalindrome(p):
 
-    # votre code ici
-    
+import string
+from unidecode import unidecode
+
+def ispalindrome(p):
+    """
+    "regarde si un mots est un palindrome et rencoit True si oui
+    """
+    palindrome = ""
+    for i in range(len(p)-1,-1,-1):
+        palindrome += p[i]
+    translator = str.maketrans(string.punctuation , ' '*len(string.punctuation)) 
+    p=p.translate(translator)
+    p=unidecode(p.replace(' ','')).lower()
+    palindrome=palindrome.translate(translator)
+    palindrome=unidecode(palindrome.replace(' ','')).lower()
+    if p == palindrome:
+        return True
     return False
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    "fonction principale qui teste les autres fonctions secondaire
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
